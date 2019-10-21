f71d60d0c168c22356618dfd9a2ea2df49287766
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-21 20:16:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,8 @@ def consolidate_cart(cart)
   cart_hash = Hash[cart.each{|item| item}]
   item_name = item.keys[0]
   item_stats = item.values[0]
+  
+  if cart_hash
 end
 
 def apply_coupons(cart, coupons)
