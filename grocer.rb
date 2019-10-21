a627dc41620b7699fc89bc7b5e6948e1a865724a
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-21 20:40:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,15 +2,14 @@ def consolidate_cart(cart)
   cart_hash = {}
   cart.each do |item|
   item_name = item.keys[0]
-  item_stats = item.values[0]
   
   if cart_hash.has_key?(item_name)
     cart_hash[item_name][:count] += 1
   else
     cart_hash[item_name] = {
       count: 1,
-      price: item_stats[:price],
-      clearance: item_stats[:clearance]
+      price: item[:price],
+      clearance: item[:clearance]
     }
   end
 end
