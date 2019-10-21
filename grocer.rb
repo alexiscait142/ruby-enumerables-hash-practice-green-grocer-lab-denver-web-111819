aa942b628ad0b90bd4fdbbd0cd0aa4e4ac65ae37
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-21 21:12:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -26,9 +26,9 @@ def apply_coupons(cart, coupons)
       cart["#{item} W/COUPON"][:count] += coupon[:num]
       cart[:item][:count] -= coupon[:num]
       
-      
     end
   end
+  cart
 end
 
 def apply_clearance(cart)
