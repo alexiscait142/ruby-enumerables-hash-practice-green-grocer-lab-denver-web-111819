13f682d0f86a1ab5085a624110beb9f4e9adb24e
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-21 21:08:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,6 +22,8 @@ def apply_coupons(cart, coupons)
     if cart[item] && cart[:item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
       cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[:item][:clearance], count: coupon[:num]}
       cart[:item][:count] -= coupon[:num]
+      elsif
+      
       
     
     end
