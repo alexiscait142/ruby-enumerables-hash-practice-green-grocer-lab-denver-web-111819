336127094d7e4bb7e38034d93081c7c0c3290bb8
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-21 20:58:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,8 +19,7 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]
-    if cart[item]
-    
+    if cart[item] && cart[:item][:count] >= coupon[:num]
     
     end
 end
